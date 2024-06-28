d0035f1f3f38b5548bc178b56c92f9094c4a1415
yvettegahamanyi <ygahamanyi26@gmail.com>
2024-06-28 22:29:31 +0200

    Added Dictionary of list of dictionaries

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -12,16 +12,16 @@ if __name__ == '__main__':
         .format(user_id)
     todos_url = "https://jsonplaceholder.typicode.com/users/{}/todos/" \
         .format(user_id)
-        
+
     user_info = requests.request('GET', user_url).json()
     todos_info = requests.request('GET', todos_url).json()
-    
+
     employee_name = user_info["name"]
     task_completed = list(filter(lambda obj:
         (obj["completed"] is True), todos_info))
     number_of_done_tasks = len(task_completed)
     total_number_of_tasks = len(todos_info)
-    
+
     print("Employee {} is done with tasks({}/{}):".format(employee_name, number_of_done_tasks, total_number_of_tasks))
-    
+
     [print("\t "+ task["title"]) for task in task_completed]

38bb98446dd2d0c72fc6bc0302fc3986099877a3
yvettegahamanyi <ygahamanyi26@gmail.com>
2024-06-28 17:37:33 +0200

    Added Gather data from an API

---
+++ api/0-gather_data_from_an_API.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Module"""
+
+import requests
+import sys
+
+"""Module"""
+
+if __name__ == '__main__':
+    user_id = sys.argv[1]
+    user_url = 'https://jsonplaceholder.typicode.com/users/{}' \
+        .format(user_id)
+    todos_url = "https://jsonplaceholder.typicode.com/users/{}/todos/" \
+        .format(user_id)
+        
+    user_info = requests.request('GET', user_url).json()
+    todos_info = requests.request('GET', todos_url).json()
+    
+    employee_name = user_info["name"]
+    task_completed = list(filter(lambda obj:
+        (obj["completed"] is True), todos_info))
+    number_of_done_tasks = len(task_completed)
+    total_number_of_tasks = len(todos_info)
+    
+    print("Employee {} is done with tasks({}/{}):".format(employee_name, number_of_done_tasks, total_number_of_tasks))
+    
+    [print("\t "+ task["title"]) for task in task_completed]
